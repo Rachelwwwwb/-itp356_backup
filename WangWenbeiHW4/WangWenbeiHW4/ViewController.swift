6e0071f09057b7903d55c941832c74f6b6acd103
Wenbei <wenbeiwa@usc.edu>
2019-10-08 16:52:35 -0700

    2.0

--- WangWenbeiHW4/WangWenbeiHW4/ViewController.swift
+++ WangWenbeiHW4/WangWenbeiHW4/ViewController.swift
@@ -11,11 +11,16 @@ import UIKit
 class ViewController: UIViewController {
     
     //create global variables to hold calculation result
-    var includeTax = false
+    var includeTax = true
     var billAmount = 0.0
-    var taxValue = 0
+    var taxValue = 0.0
+    var taxPercentage = 0.0
     var tipAmount = 0.0
     var tipPercentage = 0.0
+    var subtotal = 0.0
+    var tWithTip = 0.0
+    var splitNum = 1;
+    var tPP = 0.0
     
     @IBOutlet weak var tipSliderLabel: UILabel!
     @IBOutlet weak var splitNumLabel: UILabel!
@@ -43,7 +48,7 @@ class ViewController: UIViewController {
 
     func setDefaultValues(){
         billAmountLabel.text = ""
-        taxSegmented.selectedSegmentIndex = UISegmentedControl.noSegment
+        taxSegmented.selectedSegmentIndex = 0
         taxSlider.value = 0
         spiltStepper.value = 1
         
@@ -55,55 +60,135 @@ class ViewController: UIViewController {
         totalWithTipLabel.text = "$0.00"
         totalPPLabel.text = "$0.00"
         
-        
-        includeTax = false
+        includeTax = true
         billAmount = 0.0
-        taxValue = 0
+        taxValue = 0.0
+        subtotal = 0.0
     }
     
-    @IBAction func billAmountEntered(_ sender: UITextField) {
+    
+    @IBAction func billEntered(_ sender: UITextField) {
+        print ("hi")
         let amount = billAmountLabel.text ?? ""
         if amount.count > 0{
             billAmount = Double(amount)!
         }
-    }
-    
-    @IBAction func sliderChanged(_ sender: UISlider) {
-        if billAmountLabel.text != ""{
-            let num = sender.value
-            tipPercentage = Double(sender.value)
+        if tipPercentage != 0 {
             
-            tipSliderLabel.text = "\(Int(num))%"
             tipAmount = billAmount * (1 + tipPercentage)
-            tipLabel.text = "\(tipAmount) "
+            tipLabel.text = "$" + String (format: "%.2f", tipAmount)
+        }
+        tPP = billAmount/Double(splitNum)
+        totalPPLabel.text = "$" + String (format: "%.2f", tPP)
+    }
+        
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        let num = sender.value
+        tipPercentage = Double(sender.value/100)
+        tipSliderLabel.text = "\(Int(num))%"
+
+        if billAmountLabel.text != ""{
+            let amount = billAmountLabel.text ?? ""
+            if amount.count > 0{
+                billAmount = Double(amount)!
+            }
+            tipAmount = billAmount * (1 + tipPercentage)
+            tWithTip = billAmount + tipAmount
+            tipLabel.text = "$" + String (format: "%.2f", tipAmount)
+            if includeTax{
+                tWithTip += taxValue
+            }
+            totalWithTipLabel.text = "$" + String (format: "%.2f", tWithTip)
         }
     }
     
-    @IBAction func changeSplitNum(_ sender: UIStepper) {
+    @IBAction func taxChanged(_ sender: UISegmentedControl) {
+        switch taxSegmented.selectedSegmentIndex{
+            case 0:
+               taxPercentage = 7.5
+            case 1:
+               taxPercentage = 8
+            case 2:
+                taxPercentage = 8.5
+            case 3:
+                taxPercentage = 9
+            case 4:
+                taxPercentage = 9.5
+            default:
+                break
+        }
+        // if there's a input value
         if billAmountLabel.text != "" {
-            splitNumLabel.text = Int(sender.value).description
+            let amount = billAmountLabel.text ?? ""
+            if amount.count > 0{
+                billAmount = Double(amount)!
+        }
+        taxValue = billAmount * (taxPercentage/100.0)
+        taxLabel.text = "$" + String (format: "%.2f", taxValue)
+        }
+        
+        // change the subtotal
+        if includeTax {
+            subtotal = billAmount + taxValue
+            tWithTip = billAmount + taxValue + tipAmount
         }
         else {
-            sender.value = 1
-            splitNumLabel.text = "1"
+            subtotal = billAmount
+            tWithTip = billAmount + tipAmount
+        }
+        tPP = tWithTip / Double(splitNum)
+        subtotalLabel.text = "$" + String (format: "%.2f", subtotal)
+        totalWithTipLabel.text = "$" + String (format: "%.2f", tWithTip)
+        totalPPLabel.text = "$" + String (format: "%.2f", tPP)
+                      
+    }
+
+    @IBAction func taxSwitched(_ sender: UISwitch) {
+        if taxSwitch.isOn{
+            includeTax = true
+        }
+        else {
+            includeTax = false
+        }
+        if billAmountLabel.text != "" {
+            let amount = billAmountLabel.text ?? ""
+            if amount.count > 0{
+                billAmount = Double(amount)!
+            }
+            if (includeTax) {
+            subtotal = billAmount + taxValue
+            tWithTip = billAmount + tipAmount + taxValue
+            }
+            else {
+                subtotal = billAmount
+                tWithTip = billAmount + tipAmount
+            }
+            subtotalLabel.text = "$" + String (format: "%.2f", subtotal)
+            totalWithTipLabel.text = "$" + String (format: "%.2f", tWithTip)
+
+        }
+    }
+    
+        
+    @IBAction func changeSplitNum(_ sender: UIStepper) {
+        splitNumLabel.text = Int(sender.value).description
+        splitNum = Int(sender.value)
+        
+        if billAmountLabel.text != "" {
+            let amount = billAmountLabel.text ?? ""
+            if amount.count > 0{
+                billAmount = Double(amount)!
+            }
+            if includeTax {
+                tPP = (billAmount + tipAmount + taxValue) / Double(splitNum)
+            }
+            else {
+                tPP = (billAmount + tipAmount) / Double(splitNum)
+            }
+            totalPPLabel.text = "$" + String (format: "%.2f", tPP)
         }
 
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     
     @IBAction func clearAll(_ sender: UIButton) {
